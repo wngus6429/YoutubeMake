323156b9109374382061df1d5e73c7a5a9733dd5
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-01-25 18:14:40 +0900

    1
    1

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,12 +3,12 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-export default userRouter;
-
 userRouter.get(routes.users, (req, res) => res.send("Users"));
 userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
 userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
 userRouter.get(routes.changePassword, (req, res) => res.send("Change Password"));
+
+export default userRouter;
 // userRouter.get("/", (req, res) => res.send("user index"));
 // userRouter.get("/edit", (req, res) => res.send("user edit"));
 // userRouter.get("/password", (req, res) => res.send("user password"));
