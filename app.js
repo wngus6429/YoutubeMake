e8d63782701912907179e7ca48b22905a2281a58
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-01-25 17:05:29 +0900

    125
    125

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
 app.use(cookeParser()); //쿠키 저장기능
@@ -15,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); //보안 증가
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
