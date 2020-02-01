0a93f8e9df3c3a1f89285f2d7b35494519f0ad8b
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-02-02 04:27:45 +0900

    21
    21

---
+++ db.js
@@ -0,0 +1,55 @@
+export const videos = [
+  {
+    id: 123,
+    title: "video awesome",
+    description: "this is something i love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      //이건 사용자임
+      jid: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 1234,
+    title: "video super",
+    description: "this is something i love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      //이건 사용자임
+      jid: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 12345,
+    title: "video good",
+    description: "this is something i love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      //이건 사용자임
+      jid: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 123456,
+    title: "video perfect",
+    description: "this is something i love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      //이건 사용자임
+      jid: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  }
+];
+//이건 그냥 가짜 데이터 실험을 위한
