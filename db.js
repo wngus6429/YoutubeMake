c7e4d44b2f69df89b5aa4f65f85088fe3335a30c
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-02-03 23:33:58 +0900

    23
    23

--- db.js
+++ db.js
@@ -1,59 +1,48 @@
-export const videos = [
-  {
-    id: 123,
-    title: "video awesome",
-    description: "this is something i love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      //이건 사용자임
-      jid: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 1234,
-    title: "video super",
-    description: "this is something i love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      //이건 사용자임
-      jid: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 12345,
-    title: "video good",
-    description: "this is something i love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      //이건 사용자임
-      jid: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 123456,
-    title: "video perfect",
-    description: "this is something i love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      //이건 사용자임
-      jid: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("🏭Connected to DB");
+const handleError = error => console.log(`💀 Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
+// export const videos = [
+//   {
+//     id: 123,
+//     title: "video awesome",
+//     description: "this is something i love",
+//     views: 24,
+//     videoFile:
+//       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//     creator: {
+//       //이건 사용자임
+//       jid: 121212,
+//       name: "Nicolas",
+//       email: "nico@las.com"
+//     }
+//   },
+//   {
+//     id: 123456,
+//     title: "video perfect",
+//     description: "this is something i love",
+//     views: 24,
+//     videoFile:
+//       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//     creator: {
+//       //이건 사용자임
+//       jid: 121212,
+//       name: "Nicolas",
+//       email: "nico@las.com"
+//     }
+//   }
+// ];
 //이건 그냥 가짜 데이터 실험을 위한
--- init.js
+++ init.js
@@ -1,7 +1,11 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
-const handleListening = () => console.log(`listening on:http://localhost:${PORT}`);
+const PORT = process.env.PORT || 4000; //못 찾으면 4000으로 연결해라는거
+const handleListening = () =>
+  console.log(`listening on:http://localhost:${PORT}`);
 //application을 app파일에서 가져왔기 때문에 이렇게 할 수 있음.
 
 app.listen(PORT, handleListening);
