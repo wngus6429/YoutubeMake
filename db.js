b460c6d0e22543fd0b04e8720e54024f4f1b6013
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-02-05 00:03:51 +0900

    25
    25

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("🏭Connected to DB");
+const handleOpen = () => console.log("✅  Connected to DB");
 const handleError = error => console.log(`💀 Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
--- init.js
+++ init.js
@@ -2,10 +2,12 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT || 4000; //못 찾으면 4000으로 연결해라는거
 const handleListening = () =>
-  console.log(`listening on:http://localhost:${PORT}`);
+  console.log(`✅  Listening on: http://localhost:${PORT}`);
 //application을 app파일에서 가져왔기 때문에 이렇게 할 수 있음.
 
 app.listen(PORT, handleListening);
