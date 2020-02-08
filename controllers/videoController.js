6388e0c3fce4591a400500397db2c589c6f7c905
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-02-08 18:31:10 +0900

    28
    28

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,8 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
+    //최신꺼를 위에 배열하기 위해 뒤에 소트 사용
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -16,7 +17,7 @@ export const search = (req, res) => {
   } = req;
   // const searchingBy = req.query.term;
   // res.render("search", { pageTitle: "Search", searchingBy:searchingBy });
-  res.render("search", { pageTitle: "Search", searchingBy, videos });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
 
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
@@ -44,7 +45,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -67,14 +68,25 @@ export const postEditVideo = async (req, res) => {
     body: { title, description }
   } = req;
   try {
-    await Video.findOneAndUpdate({ id }, { title, description }); //뒤에 부분이 수정하고 싶은곳
+    await Video.findOneAndUpdate({ _id: id }, { title, description }); //뒤에 부분이 수정하고 싶은곳
+    console.log(video);
     res.redirect(routes.videoDetail(id));
   } catch (error) {
     res.redirect(routes.home);
   }
 };
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect(routes.home);
+};
 
-//여기가 MVC에서 C부분임
-//Pug로 V부분도 할 예정임
+// 여기가 MVC에서 C부분임
+//pageTitle 이건 말 그대로 페이지 타이틀임
