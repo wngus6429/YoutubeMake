e7fb7610cf5574c637ce1b03f05ee044068e6bbc
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-02-13 22:45:50 +0900

    213
    213

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,22 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { getJoin, getLogin, postJoin, postLogin, logout } from "../controllers/userController";
+import {
+  getJoin,
+  getLogin,
+  postJoin,
+  postLogin,
+  logout
+} from "../controllers/userController";
 //현재 폴더의 바깥으로 지정해야 해서 .. 두개 붙임.
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, getJoin); //아이디 만듬
+globalRouter.post(routes.join, postJoin); //아이디 만듬 정보 가림
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, getLogin); //로그인 함
+globalRouter.post(routes.login, postLogin); //로그인 정보 가림
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
