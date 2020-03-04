3ca5c9c2e92dacddde2020ac392ce06728b93fd6
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-03-05 00:17:43 +0900

    35
    35

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,17 +2,46 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumnBtn = document.getElementById("jsVolumnBtn");
+const fullScreenBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="far fa-pause-circle"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fab fa-gitlab"></i>';
   }
+}
+
+function handleVolumnClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumnBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumnBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
+function goFullScreen() {
+  videoContainer.webkitRequestFullscreen(); //고유의 기능
+  fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenBtn.removeEventListener("click", goFullScreen);
+  fullScreenBtn.addEventListener("click", exitFullScreen);
+}
+
+function exitFullScreen() {
+  fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenBtn.addEventListener("click", goFullScreen);
+  document.webkitExitFullscreen(); //고유의 기능
 }
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumnBtn.addEventListener("click", handleVolumnClick);
+  fullScreenBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
