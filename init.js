1ab028dfcf93658a362103cf57eeece11e669bbd
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-08-21 01:51:03 +0900

    821
    821

--- init.js
+++ init.js
@@ -1,16 +1,14 @@
 import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-
-dotenv.config();
-
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
 
+dotenv.config();
+
 const PORT = process.env.PORT || 4000; //못 찾으면 4000으로 연결해라는거
-const handleListening = () =>
-  console.log(`✅  Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅  Listening on: http://localhost:${PORT}`);
 //application을 app파일에서 가져왔기 때문에 이렇게 할 수 있음.
 
 app.listen(PORT, handleListening);
